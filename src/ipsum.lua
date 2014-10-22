90088a45a70e287b41266f3c5e427b22f323342a
Chris <anfractuosity@anfractuosity.com>
2014-10-22 23:20:29 +0200

    json support

--- src/ipsum.lua
+++ src/ipsum.lua
@@ -3,24 +3,96 @@ local key = require ("keylogging")
 local mouse = require("mouselogging")
 local xorg = require("xorg")
 
+lasthash = {["tmp"]="hi"}
+
+local lind = lanes.linda()
+local lock = lanes.genlock(lind,"M",1)
+
+
 function callback(evtid,value)
+	
+	function clear(l)
+		d = l:dump()
+		if d == nil then
+			return
+		end
+		for k,v in pairs(d) do
+			l:set(k,nil)
+		end
+	end
+
+
+	c = 0
+	for k,v in pairs(lasthash) do
+		c = c + 1
+	
+	end
+
+	print("HASH LEN ",c)
 	local utils = require("utils")
 	if evtid == 1 or evtid == 2 then
-		print("Trying to grab top")	
 		win = utils.gettopwindow()
-		print("Hanging?")
 		if win ~= nil then
-			print(">>>>>>>>>>>>>   ",win["windowname"])
+			lock(1)
+			if not lind:get(win["windowid"]) then
+				print(">>>>>>>>>>>>> ",win["windowname"])
+			else
+				print("HEEEEEEEEEEEEEEEREEEEEEEEEE")
+			end
+			
+			--lasthash = {}
+			clear(lind)
+			lind.set(win["windowid"],1)
+			lock(-1)
+
 		else
 			print("Failed to grab window")
 		end
 	end
+	if evtid == 3 then
+		lock(1)
+	               	 
+			if lind:get(value["windowid"]) ~= nil then                                                           
+                                print(">>>>>>>>>>>>> ",value["windowname"])                                               
+                        else                                                                                            
+                                print("HEEEEEEEEEEEEEEEREEEEEEEEEE")                                                    
+                        end                                                                                             
+                                                                                                                        
+                        --lasthash = {}                                                                                 
+                        clear(lind)                                                                                     
+                        lind.set(value["windowid"],1)  	
+		--lasthash = {}
+		clear(lind)
+		lock(-1)
+	end
 	
 end
 
-threadkey = lanes.gen("*",{},key.grabber)
-threadmouse = lanes.gen("*",{globals={["mouse"]=mouse}},mouse.grabber)
-threadxorg = lanes.gen("*",{globals={["xorg"]=xorg}},xorg.grabber)
+glob={["lasthash"]=lasthash, 
+      ["lind"]=lind,
+     ["lock"]=lock
+     }
+
+
+
+
+JSON = require("JSON") 
+
+
+local settings = JSON:decode([[{"hi":"bob"}]]) -- decode example
+
+
+print(settings["hi"])
+
+
+
+
+
+
+
+threadkey = lanes.gen("*",{globals=glob},key.grabber)
+threadmouse = lanes.gen("*",{globals=glob},mouse.grabber)
+threadxorg = lanes.gen("*",{globals=glob},xorg.grabber)
 
 r1 = threadkey(callback)
 r2 = threadmouse(callback)
