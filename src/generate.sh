3bc3679bdc8f702c144522b93255164fb8b48e9f
Chris <anfractuosity@anfractuosity.com>
2014-10-25 15:16:34 +0200

    luajit

---
+++ src/generate.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+declare -A memstart
+declare -A memsize
+
+cp *.lua lua
+find luasocket/src -name "*.lua" | xargs -I {} cp {} lua/
+find lualanes/src -name "*.lua" | xargs -I {} cp {} lua/   
+
+cd lua
+
+rm *.o
+ls *.lua | xargs -I {} ld -r -b binary -o "{}".o "{}"  
+
+rm ext.h
+rm loadlua.c
+
+for f in *.lua.o 
+do
+	name=$(echo $f|sed "s/\.lua\.o$//g") 
+	textaddr=$(objdump -t "$f"  | egrep "_start$" | awk '{ print $5 }')
+	echo "extern char $textaddr;" >> ext.h
+	memstart[$name]=$textaddr
+        textsize=$(objdump -t "$f"  | egrep "_size$" | awk '{ print $5 }')
+	echo "extern char $textsize;" >> ext.h
+
+        #textend=$(objdump -t "$f"  | egrep "_end$" | awk '{ print $5 }')
+        #echo "extern char $textend;" >> ext.h
+
+	memsize[$name]=$textsize    
+done
+
+for f in *.lua.o 
+do
+
+	name=$(echo $f|sed "s/\.lua\.o$//g")
+	textaddr=${memstart[$name]}
+	textsize=${memsize[$name]}
+
+	echo -e "lua_getglobal(L, \"package\");
+lua_getfield(L, -1, \"preload\");
+luaL_loadbuffer(L,&$textaddr,(uintptr_t)&$textsize,\"$name\");
+lua_setfield(L, -2, \"$name\");
+lua_pop(L, 2);" >> loadlua.c
+	
+done
